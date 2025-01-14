26a5e8c7f03749bd759e720cd228793b59ebd5ec
lguille1991 <guillermoabrego@ravn.co>
2025-01-14 16:27:52 -0600

    Added sample assertions

--- tests/challenge/swaglabs-challenge.spec.js
+++ tests/challenge/swaglabs-challenge.spec.js
@@ -7,14 +7,30 @@ test.describe('Navigate to the Swag Labs Login Page', () => {
     });
 
     test('Login using valid credentials - Challenge', async ({ page }) => {
+
+        // Your test logic goes here
         
+
+        // You can modify this assetion or add more
+        await expect(page).toHaveURL(/.*inventory/);
     });
 
     test('Login attempt using invalid credentials - Challenge', async ({ page }) => {
+
+        // Your test logic goes here
         
+
+        // You can modify this assetion or add more
+        await expect(page.getByText('Epic sadface')).toBeVisible();
     });
 
     test('Place an order - Challenge', async ({ page }) => {
+
+        // Your test logic goes here
         
+
+        // You can modify this assetion or add more
+        await expect(page.getByText('THANK YOU FOR YOUR ORDER')).toBeVisible();
+        await expect(page).toHaveURL(/.*checkout-complete/);
     });
 });
