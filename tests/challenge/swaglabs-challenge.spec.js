cda0ea574c7d66833e3ae96fb8b0809e45a59f3f
MarcelaLopez <marc3lyn@gmail.com>
2025-05-30 12:44:01 -0600

    adding test Place an order - Challenge

--- tests/challenge/swaglabs-challenge.spec.js
+++ tests/challenge/swaglabs-challenge.spec.js
@@ -9,10 +9,10 @@ test.describe('Navigate to the Swag Labs Login Page', () => {
     test('Login using valid credentials - Challenge', async ({ page }) => {
 
         // Your test logic goes here
-        await page.getByRole('textbox',{name:'Username'}).fill('standard_user');
+        await page.getByRole('textbox', { name: 'Username' }).fill('standard_user');
         await page.locator('[data-test="password"]').fill('secret_sauce');
-        await page.locator("//input[@data-test='login-button']").click();    
-        
+        await page.locator("//input[@data-test='login-button']").click();
+
 
         // You can modify this assetion or add more
         await expect(page).toHaveURL(/.*inventory/);
@@ -21,10 +21,10 @@ test.describe('Navigate to the Swag Labs Login Page', () => {
     test('Login attempt using invalid credentials - Challenge', async ({ page }) => {
 
         // Your test logic goes here
-        await page.getByRole('textbox',{name:'Username'}).fill('standard_user');
-        await page.locator('[data-test="password"]').fill('invalid');
+        await page.getByRole('textbox', { name: 'Username' }).fill('standard_user');
+        await page.getByRole('[data-test="password"]').fill('invalid');
         await page.locator("//input[@data-test='login-button']").click();
-        
+
 
         // You can modify this assetion or add more
         await expect(page.getByText('Epic sadface')).toBeVisible();
@@ -33,7 +33,42 @@ test.describe('Navigate to the Swag Labs Login Page', () => {
     test('Place an order - Challenge', async ({ page }) => {
 
         // Your test logic goes here
-        
+        // Log in
+        await page.getByRole('textbox', { name: 'Username' }).fill('standard_user');
+        await page.getByRole('textbox', { name: 'password' }).fill('secret_sauce');
+        await page.getByRole('button', { name: 'Login' }).click();
+
+        // Select a item
+        const items = page.locator('#inventory_container .inventory_item_name');
+        const itemsCount = await items.count();
+
+        console.log('Total elementos: ' + itemsCount);
+        const randomIndex = Math.floor(Math.random() * itemsCount);
+        console.log('random index: ' + randomIndex);
+
+        const itemSelected = await items.nth(randomIndex);
+
+        const productName = await itemSelected.innerText();
+        console.log('Producto seleccionado: ' + productName);
+
+        await itemSelected.click();
+
+        //Add to cart the selected item
+        await page.locator('button:has-text("Add to cart")').click();
+
+
+        await page.locator('#shopping_cart_container').click();
+        await page.getByText('Checkout').click();
+        await page.getByText('Continue').click();
+
+        // Fill form
+        await page.locator('[data-test="firstName"]').fill('Juan');
+        await page.locator('[data-test="lastName"]').fill('Perez');
+        await page.locator('[data-test="postalCode"]').fill('12345');
+
+        // Complete process
+        await page.getByRole('button', { name: 'Continue' }).click();
+        await page.getByRole('button', { name: 'Finish' }).click();
 
         // You can modify this assetion or add more
         await expect(page.getByText('THANK YOU FOR YOUR ORDER')).toBeVisible();
