b52fbb0102d47f15f8789fc3d6fc7d8a7bd75bfc
WilfredoAlfaro <156016579+WilfredoAlfaro@users.noreply.github.com>
2025-08-22 13:43:26 -0600

    Add new challenge structure (#5)

---
+++ src/pages/page1.js
@@ -0,0 +1,11 @@
+import BasePage from "./basepage.js";
+class Page1 extends BasePage {
+  constructor(Page) {
+    super(Page);
+    this.page = Page;
+    //Add locators here
+  }
+  //Add actions functions here
+}
+
+export default Page1;
